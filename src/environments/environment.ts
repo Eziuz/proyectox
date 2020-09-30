3583afc7752e2c83fd3b75f0bc2d8686fa880dff
Eziuz <camilojimenez160@gmail.com>
2020-09-30 17:59:35 -0500

    Proyecto Final

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dataServiceUrl: 'http://localhost:8080/HemotecSOA-1.0/HemotecApi'
 };
 
 /*
